9a953cf0b4eb13360c3e555903bb9ac65f8fec04
tebit <tibet.tb@gmail.com>
2023-10-13 14:44:07 +0700

    Made a timer on Chat Box page to where if no messages get sent within 15 seconds, the page would change back to landing page

--- lib/src/features/chat_box/app/chat_box.dart
+++ lib/src/features/chat_box/app/chat_box.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:rbc_interface/src/features/landing_page/app/landing_page.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
@@ -9,34 +11,64 @@ class ChatBox extends StatefulWidget {
   State<ChatBox> createState() => _ChatBoxState();
 }
 
-class _ChatBoxState extends State<ChatBox>{
-  
+class _ChatBoxState extends State<ChatBox> {
   List<types.Message> _messages = [];
-  final _user = const types.User(
-    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',);
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
+  Timer? _inactiveTimer;
 
   @override
-  Widget build(BuildContext context){
+  void initState() {
+    super.initState();
+    _startInactiveTimer();
+  }
+
+  @override
+  void dispose() {
+    _inactiveTimer?.cancel();
+    super.dispose();
+  }
+
+  void _startInactiveTimer() {
+  _inactiveTimer = Timer(Duration(seconds: 15), () {
+    if (mounted) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LandingPage()),
+      );
+    }
+  });
+  }
+
+
+  void _resetInactiveTimer() {
+    _inactiveTimer?.cancel();
+    _startInactiveTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Chat(messages: _messages, onSendPressed: _handleSendPressed, user: _user),
+      body: GestureDetector(
+        onTap: _resetInactiveTimer,
+        child: Chat(messages: _messages, onSendPressed: _handleSendPressed, user: _user),
+      ),
     );
   }
 
   void _handleSendPressed(types.PartialText message) {
-      final textMessage = types.TextMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: "id",
-        text: message.text,
-      );
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: "id",
+      text: message.text,
+    );
 
-      _addMessage(textMessage);
-    }
-    
+    _addMessage(textMessage);
+    _resetInactiveTimer(); // Reset the timer on send
+  }
+
   void _addMessage(types.Message message) {
     setState(() {
       _messages.insert(0, message);
     });
   }
-
 }
